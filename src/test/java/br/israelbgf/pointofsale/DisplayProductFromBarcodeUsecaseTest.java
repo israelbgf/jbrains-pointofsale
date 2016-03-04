0a98102c7b7a8486177eb59079523d7b413f8f0f
Israel Fonseca <israel.bgf@gmail.com>
2016-03-04 07:56:12 -0300

    Removed duplication.

--- src/test/java/br/israelbgf/pointofsale/DisplayProductFromBarcodeUsecaseTest.java
+++ src/test/java/br/israelbgf/pointofsale/DisplayProductFromBarcodeUsecaseTest.java
@@ -1,5 +1,6 @@
 package br.israelbgf.pointofsale;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -7,21 +8,24 @@ import static org.junit.Assert.assertThat;
 
 public class DisplayProductFromBarcodeUsecaseTest {
 
-    @Test
-    public void validationOfEmptyBarcode(){
-        DisplayProductFromBarcodePresenterSpy presenterSpy = new DisplayProductFromBarcodePresenterSpy();
-        DisplayProductFromBarcodeUsecase usecase = new DisplayProductFromBarcodeUsecase(presenterSpy);
+    private DisplayProductFromBarcodePresenterSpy presenterSpy;
+    private DisplayProductFromBarcodeUsecase usecase;
 
+    @Before
+    public void setUp() {
+        presenterSpy = new DisplayProductFromBarcodePresenterSpy();
+        usecase = new DisplayProductFromBarcodeUsecase(presenterSpy);
+    }
+
+    @Test
+    public void validationOfEmptyBarcode() {
         usecase.process("");
 
         assertThat(presenterSpy.spiedText, equalTo("ERROR: Empty Barcode."));
     }
 
     @Test
-    public void validationOfNotANumberBarcode(){
-        DisplayProductFromBarcodePresenterSpy presenterSpy = new DisplayProductFromBarcodePresenterSpy();
-        DisplayProductFromBarcodeUsecase usecase = new DisplayProductFromBarcodeUsecase(presenterSpy);
-
+    public void validationOfNotANumberBarcode() {
         usecase.process("12JBC2");
 
         assertThat(presenterSpy.spiedText, equalTo("ERROR: Invalid Barcode."));
